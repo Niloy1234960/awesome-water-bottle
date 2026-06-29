deeab502dee67925a14e5883b107b1b54a211acb
Niloy Mondal <niloymondal1370@gmail.com>
2026-06-29 10:56:06 +0600

    implement save and get data from localstorage

--- src/components/Bottles.jsx
+++ src/components/Bottles.jsx
@@ -1,15 +1,38 @@
-import React, { use, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import Bottle from "./Bottle";
+import { addItemToLocalStorage, getCartFromLocalStoreage } from "../utilities/AddToLocalStorage";
 
 const Bottles = ({ bottlesPromise }) => {
   const [cart, setCart] = useState([]);
 
   const bottles = use(bottlesPromise);
+  
+  // Use useEffect 
+  useEffect(()=>{
+    const storedItem = getCartFromLocalStoreage()
+    // console.log(storedItem);
+    
+    const storedCart = []
+
+    for(const id of storedItem){
+      // console.log(id);
+      const cartBottle = bottles.find(bottle => bottle.id === id)
+      if(cartBottle){
+        storedCart.push(cartBottle)
+      }
+    }
+
+    // console.log("stored cart", storedItem);
+    setCart(storedItem)
+
+  }, [])
 
   const handleAddToCart = (bottle) => {
     // console.log("Btn clicked", bottle);
     const newCart = [...cart, bottle];
     setCart(newCart)
+
+    addItemToLocalStorage(bottle.id)
   };
 
   return (
---
+++ src/utilities/AddToLocalStorage.js
@@ -0,0 +1,22 @@
+const getCartFromLocalStoreage = () => {
+    const storedCartString = localStorage.getItem('cart')
+    if(storedCartString){
+        const storedCart = JSON.parse(storedCartString)
+        return storedCart;
+    }
+    return []
+}
+
+const saveCartToLocalStorage = (cart) => {
+    const cartStrigified = JSON.stringify(cart)
+    localStorage.setItem("cart", cartStrigified)
+}
+
+const addItemToLocalStorage = (id) => {
+    const cart = getCartFromLocalStoreage()
+    const newCart =[...cart, id]
+
+    saveCartToLocalStorage(newCart)
+}
+
+export {getCartFromLocalStoreage, addItemToLocalStorage}
